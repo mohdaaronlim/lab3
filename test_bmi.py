8a34b40f28760bbd35879046ea2fa00d97270e7b
mohdaaronlim <mohdaaron.22@ichat.sp.edu.sg>
2023-11-05 15:27:04 +0800

    Ex1

---
+++ test_bmi.py
@@ -0,0 +1,24 @@
+import lab2.Ex1.bmi as bmi
+
+def test_bmi_normal_weight():
+    test = 0
+
+    result = bmi.calculate_bmi(1.2, 30)
+
+    assert (result == test)
+
+
+def test_bmi_over_weight():
+    test = 1
+
+    result = bmi.calculate_bmi(1.2, 50)
+
+    assert (result == test)
+
+
+def test_bmi_under_weight():
+    test = -1
+
+    result = bmi.calculate_bmi(1.2, 25)
+
+    assert (result == test)
